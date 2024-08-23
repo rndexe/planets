fe0e22db990bba4ae1d6eaff98b1212adb71d516
rndexe <himanshu.ernde@gmail.com>
2024-08-23 18:51:11 +0530

    change github workflow

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 export default defineConfig({
-    base: "/planets/",
+    base: process.env.BASE_PATH ?? '',
     plugins: [svelte()],
 });
